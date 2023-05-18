885c4b76da709ce25d5675117fbcefc7a2787290
Dehemi Weerakkody <dehemiweerakkodi2@gmail.com>
2023-05-19 03:21:08 +0530

    Added tests

---
+++ src/pages/ReservationPage/__tests__/ReservationForm.test.jsx
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import ReservationForm from '../ReservationForm';
+
+import { useNavigate, useParams } from 'react-router-dom'; // Import the module you want to test
+import '@testing-library/jest-dom/extend-expect';
+jest.mock('react-router-dom'); 
+
+describe('Tests for Reservation form', () => {
+  useParams.mockReturnValue({ room_id: '123' });
+  test('displays error message if terms and conditions checkbox is not checked', async () => {
+    render(<ReservationForm />);
+    const submitButton = screen.getByText('Submit');
+    fireEvent.click(submitButton);
+    await act(async () => {});
+    const errorMessage = screen.getByText('You must accept the terms and conditions');
+    expect(errorMessage).toBeInTheDocument();
+  });
+});
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
+  resolve: {
+    alias: {
       // '@' : path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components/'),
-      '@schema': path.resolve(__dirname, './src/schema/'),
-      '@pages': path.resolve(__dirname, './src/pages/'),
-      '@utils': path.resolve(__dirname, './src/utils/')
+      "@components": path.resolve(__dirname, "./src/components/"),
+      "@schema": path.resolve(__dirname, "./src/schema/"),
+      "@pages": path.resolve(__dirname, "./src/pages/"),
+      "@utils": path.resolve(__dirname, "./src/utils/"),
     },
   },
+  optimizeDeps: {
+    exclude: ["@babel/preset-env", "@babel/preset-react", "babel-jest"],
+  },
   plugins: [react()],
-})
+  test: {
+    environment: "happy-dom",
+  },
+});
