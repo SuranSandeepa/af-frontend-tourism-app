1920626936497d963655e993a70f415d20286045
Dehemi Weerakkody <dehemiweerakkodi2@gmail.com>
2023-05-18 20:38:36 +0530

    Accomodation crud

---
+++ src/components/AccomodationEditor copy 2/AccomodationEditor.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+
+function AccomodationEditor({ onClickClose }) {
+  return (
+    <form className="w-full max-w-lg bg-white p-4 rounded-lg relative">
+      <button className="p-2 absolute top-0 right-0" onClick={onClickClose}>
+        <AiOutlineClose />
+      </button>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="provider-address"
+          >
+            Provider Address
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="provider-address"
+            type="text"
+            placeholder="Enter provider address"
+          />
+        </div>
+        <div className="w-full md:w-1/2 px-3">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="price"
+          >
+            Price
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="price"
+            type="text"
+            placeholder="Enter price"
+          />
+        </div>
+        <div className="w-full md:w-1/2 px-3">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="price"
+          >
+            Provider Name
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="price"
+            type="text"
+            placeholder="Enter price"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="description"
+          >
+            Description
+          </label>
+          <textarea
+            className="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="description"
+            placeholder="Enter description"
+          ></textarea>
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3">
+        <div className="w-full px-3">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Add Accommodation
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default AccomodationEditor;
