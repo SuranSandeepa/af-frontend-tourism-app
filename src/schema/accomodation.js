c55aeebf2f55bcc7c6f64bc9794db8dedab7e2b1
Dehemi Weerakkody <dehemiweerakkodi2@gmail.com>
2023-05-20 03:21:52 +0530

    Schema update

--- src/schema/accomodation.js
+++ src/schema/accomodation.js
@@ -2,13 +2,23 @@ import * as yup from "yup";
 
 export const reserveFormSchema = yup.object().shape({
   email: yup.string().email().label("Email").required(),
-  checkInDate:yup.string().required(),
-  checkOutDate: yup.string().required(),
-  guests: yup.number().required(),
-  specialRequests: yup.string(),
-  fullName: yup.string().required(),
-  email: yup.string().required(),
-  phone: yup.string().required(),
-  address: yup.string(),
-  termsAgree: yup.boolean().oneOf([true], 'You must accept the terms and conditions')
+  checkInDate: yup.string().label("Check in Date").required(),
+  checkOutDate: yup.string().label("Check out Date").required(),
+  guests: yup.number().label("Guests").required(),
+  specialRequests: yup.string().label("Special Requests"),
+  fullName: yup.string().label("Full Name").required(),
+  phone: yup.string().label("Phone").required(),
+  address: yup.string().label("Address"),
+  termsAgree: yup
+    .boolean()
+    .oneOf([true], "You must accept the terms and conditions"),
+});
+
+export const accomadationEditorSchema = yup.object().shape({
+  provider: yup.string().required(),
+  address: yup.string().required(),
+  price: yup.number().required(),
+  desc: yup.string(),
+  guests: yup.string().required(),
+  name: yup.string().required(),
 });
